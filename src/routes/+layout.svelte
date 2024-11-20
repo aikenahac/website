0b5f63f21e8884bdf19b18ba2729e597b7684c55
Aiken Tine Ahac <ahac.aiken@gmail.com>
2024-11-20 19:09:02 +0100

    update adapter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import Nav from '$lib/components/Nav.svelte';
 	import { t } from '$lib/translations';
 	import '../app.css';
-	
+
 	let { children } = $props();
 </script>
 
@@ -10,18 +10,19 @@
 	<title>Aiken Tine Ahac</title>
 </svelte:head>
 
-<main class="flex flex-col items-center p-12 max-[860px]:p-6 bg-aiblack max-w-screen min-h-screen">
+<main
+	class="max-w-screen flex min-h-screen flex-col items-center bg-aiblack p-12 max-[860px]:p-6"
+>
 	<Nav />
 	{@render children()}
 	<div
-	class="mt-8 flex w-full flex-col items-center justify-center gap-4 rounded-sm border border-aipink p-6"
->
-	<p class="font-mono text-aipink">{$t('common.made')}</p>
-	<a
-		class="font-mono text-aipink underline"
-		href="https://github.com/aikenahac/website"
-		target="_blank">{$t('common.oss')}</a
+		class="mt-8 flex w-full flex-col items-center justify-center gap-4 rounded-sm border border-aipink p-6"
 	>
-</div>
-
+		<p class="font-mono text-aipink">{$t('common.made')}</p>
+		<a
+			class="font-mono text-aipink underline"
+			href="https://github.com/aikenahac/website"
+			target="_blank">{$t('common.oss')}</a
+		>
+	</div>
 </main>
